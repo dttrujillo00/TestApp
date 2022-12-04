964bc475af32dc38725a7daec1ec00a4357658f5
Daniel Tamayo <dttrujillo00@gmail.com>
2022-12-04 01:32:20 -0500

    Routing entre Login y Dashboard
    Configuraciones basicas para navegar desde Login hacia Dasboard

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
 import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +17,7 @@ export class LoginComponent {
   mode: ProgressSpinnerMode = 'indeterminate';
   loading = false;
 
-  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar){
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar, private router: Router){
     this.form = this.fb.group({
       usuario: ['', Validators.required],
       password: ['', Validators.required]
@@ -48,7 +49,7 @@ export class LoginComponent {
   simulLoading(){
     this.loading = true;
     setTimeout(() => {
-      this.loading = false;
+      this.router.navigate(['dashboard'])
     }, 2000);
   }
 
